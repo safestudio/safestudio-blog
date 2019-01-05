7d34a2156354feaed2a0c352510a8eba8fa525c4
Le Minh Tri <ansidev@gmail.com>
2019-01-05 11:00:38 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container h-100">
+    <div class="row align-items-center h-100">
+      <div class="col-12 mx-auto">
+        <div class="jumbotron text-center">
+          <app-logo/>
+          <h1 class="display-4">SAFE Studio Blog</h1>
+          <p class="lead"><span class="highlight">SAFE</span> = <span class="highlight">S</span>oftware <span class="highlight">A</span>pplication <span class="highlight">F</span>or <span class="highlight">E</span>veryone</p>
+          <p class="lead">&copy; 2019 by <strong>SAFE Studio Team</strong></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style>
+#__layout {
+  height: 100vh;
+}
+.jumbotron {
+  background-color: #fff;
+}
+.highlight {
+  color: blue;
+  font-weight: 700;
+}
+</style>
+
